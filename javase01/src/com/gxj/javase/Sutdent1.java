a17f21ea6479591b8548d5e76477900bc8b58e5b
GXJ521 <152963339@.qq.com>
2022-07-03 20:40:54 +0800

    不同的人第二次提交代码

--- javase01/src/com/gxj/javase/Sutdent1.java
+++ javase01/src/com/gxj/javase/Sutdent1.java
@@ -1,4 +1,8 @@
 package com.gxj.javase;
 
 public class Sutdent1 {
+    public static void main(String[] args) {
+        System.out.println("123123");
+        System.out.println("123123");
+    }
 }
